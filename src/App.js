18e5899e8eb39423507ce50f7b9c7ccc0856c3b4
pawankumar03 <ashis.mohanty@talentedge.in>
2020-06-01 00:56:08 +0530

    adding file to git hub

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
+import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
+class App extends Component {
+  constructor() {
+    super()
+  
+    this.state = {
+       monsters:[],
+       searchfield: ''
+    }
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(Response =>Response.json())
+    .then(users => this.setState({monsters:users}))
+  }
+  render() {
+    const {monsters,searchfield} =this.state;
+    const filterMonsters = monsters.filter(
+          monster =>monster.name.toLowerCase().includes(searchfield.toLowerCase()));
+    return (
+      <div className="App">
+        <h1>Monsters List</h1>
+        <SearchBox placeholder="search monster" 
+                  handelChange={e=>this.setState({searchfield:e.target.value})}></SearchBox>
+        <CardList monsters={filterMonsters} > 
+        
+        </CardList>
+        
+      </div>
+    )
+  }
+}
+
+export default App
+
+
+/*function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -23,4 +61,4 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './search-box.styles.css';
+
+export const SearchBox = ({placeholder,handelChange}) =>(
+    <input type="search" className="search"
+            placeholder={placeholder} 
+            onChange={handelChange} />
+)
